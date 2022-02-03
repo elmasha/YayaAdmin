d00e6fbbf01bed70651c40c52c8b56edc1e2c7ba
user <elmashavitran@gmail.com>
2022-02-03 14:35:35 +0300

    first commit

---
+++ src/components/SideBar.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    app
+    color="#1C1B2B"
+    mini-variant
+    mini-variant-width="90"
+  >
+    <h4 class="d-block text-center mx-auto mt-4" size="60" style="color: #0bf4de">
+      Admin
+    </h4>
+    <v-list flat class="mt-16" dark>
+      <v-list-item-group v-model="selectItem">
+        <v-list-item active-class="border" :v-slot="{ active }" ripple="false">
+          <font-awesome-icon icon="chart-line" />
+          <span>Overview</span>
+        </v-list-item>
+        <v-list-item active-class="border" :v-slot="{ active }" ripple="false">
+          <span>Bureau</span>
+        </v-list-item>
+        <v-list-item active-class="border" :v-slot="{ active }" ripple="false">
+          <span>Clients</span>
+        </v-list-item>
+        <v-list-item active-class="border" :v-slot="{ active }" ripple="false">
+          <span>Auth</span>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    selectItem: 0,
+    drawer: null,
+    items: [
+      { icon: require("@/assets/img/account.png") },
+      { icon: require("@/assets/img/account.png") },
+      { icon: require("@/assets/img/account.png") },
+      { icon: require("@/assets/img/account.png") },
+    ],
+  }),
+};
+</script>
+
+<style>
+.border {
+  text-decoration: none;
+  text-shadow: 1px 1px 2px black, 0 0 15px #0bf4de, 0 0 5px aqua;
+}
+.v-list-item-group .v-list-item-active {
+  color: #0bf4de !important;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Dashboard from '@/components/Dashboard.vue'
+
+
+
+Vue.use(VueRouter)
+
+const routes = [{
+        path: '/',
+        name: 'dashboard',
+        component: Dashboard
+    },
+
+]
+
+const router = new VueRouter({
+    routes
+})
+
+export default router
