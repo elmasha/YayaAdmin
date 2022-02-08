db615426a2322d8f749caf1d74321bea35ffd6f1
user <elmashavitran@gmail.com>
2022-02-08 16:33:54 +0300

    34

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -121,7 +121,36 @@
             </v-card>
           </b-col>
           <!-- Col 3-->
-          <b-col sm>col-sm</b-col>
+          <b-col sm
+            ><v-card class="mt-4 mx-auto" max-width="600">
+              <v-sheet
+                class="v-sheet--offset mx-auto"
+                color="cyan"
+                elevation="0"
+                max-width="calc(100% - 32px)"
+              >
+                <v-sparkline
+                  :labels="Helpers"
+                  value="1000"
+                  color="white"
+                  line-width="3"
+                  padding="16"
+                ></v-sparkline>
+              </v-sheet>
+
+              <v-card-text class="pt-0">
+                <div class="text-h6 font-weight-light mb-2">User Registrations</div>
+                <div class="subheading font-weight-light grey--text">
+                  Last Campaign Performance
+                </div>
+                <v-divider class="my-2"></v-divider>
+                <v-icon class="mr-2" small> mdi-clock </v-icon>
+                <span class="text-caption grey--text font-weight-light"
+                  >last registration 26 minutes ago</span
+                >
+              </v-card-text>
+            </v-card></b-col
+          >
         </b-row>
       </div>
     </v-content>
@@ -254,7 +283,7 @@ body {
   margin: 6px;
 }
 .b-row {
-  margin-top: 20px;
+  background-color: #1c1b2b;
 }
 .data-h3 {
   text-decoration: none;
